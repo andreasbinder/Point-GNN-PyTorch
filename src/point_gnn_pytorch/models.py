3571009037e6645a9ae4ad415564de5b3d07a55d
Andreas Binder <andreasjosef.binder@tum.de>
2022-12-16 13:30:56 +0100

    increase model size

--- src/point_gnn_pytorch/models.py
+++ src/point_gnn_pytorch/models.py
@@ -20,28 +20,52 @@ class PointGNN(torch.nn.Module):
         self.num_node_features = num_node_features
         self.num_classes = num_classes
 
-        self.conv1 = PointGNNConv(mlp_h=MLP(MLP_h), mlp_f=MLP(MLP_f), mlp_g=MLP(MLP_g), aggr="max")
-        self.conv2 = PointGNNConv(mlp_h=MLP(MLP_h), mlp_f=MLP(MLP_f), mlp_g=MLP(MLP_g), aggr="max")
-        self.conv3 = PointGNNConv(mlp_h=MLP(MLP_h), mlp_f=MLP(MLP_f), mlp_g=MLP(MLP_g), aggr="max")
+        lin_hidden_space = [300, 300]
 
-        self.linear = nn.Linear(num_node_features, num_classes)
+        mlp_h_space = [300, 3]
+        mlp_f_space = [303, 300]
+        mlp_g_space = [300, 300]
+
+        embedd_space = [3, 64, 128, 300]
+        self.project = MLP(embedd_space)
+
+        self.conv1 = PointGNNConv(mlp_h=MLP(mlp_h_space), mlp_f=MLP(mlp_f_space), mlp_g=MLP(mlp_g_space), aggr="max")
+        self.conv2 = PointGNNConv(mlp_h=MLP(mlp_h_space), mlp_f=MLP(mlp_f_space), mlp_g=MLP(mlp_g_space), aggr="max")
+        self.conv3 = PointGNNConv(mlp_h=MLP(mlp_h_space), mlp_f=MLP(mlp_f_space), mlp_g=MLP(mlp_g_space), aggr="max")
+
+        lin_hidden_space = [300, 300]
+        self.lin1 = MLP(lin_hidden_space)
+        self.lin2 = MLP(lin_hidden_space)
+        self.lin3 = MLP(lin_hidden_space)
+
+        self.decision = nn.Linear(300, num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         pos = x
 
+        x = self.project(x)
+
         x = self.conv1(x, pos, edge_index)
         x = F.relu(x)
+        x = self.lin1(x)
 
         x = F.dropout(x, training=self.training)
+
         x = self.conv2(x, pos, edge_index)
         x = F.relu(x)
+        x = self.lin2(x)
+
         x = F.dropout(x, training=self.training)
+
         x = self.conv3(x, pos, edge_index)
+        x = F.relu(x)
+        x = self.lin3(x)
+
 
         x = global_mean_pool(x, data.batch)
 
-        x = self.linear(x)
+        x = self.decision(x)
 
         return F.log_softmax(x, dim=1)
 
